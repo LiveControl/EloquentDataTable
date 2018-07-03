eead1f1a6e5637c0c459329b8eb4d8b45c42634f
Jeffrey de Vreede <jeffrey@devreede.eu>
2018-07-03 16:18:51 +0200

    Fix count for models with globals scopes.
    If the query is based on a model with global scope the count was wrong.

--- src/LiveControl/EloquentDataTable/DataTable.php
+++ src/LiveControl/EloquentDataTable/DataTable.php
@@ -134,8 +134,15 @@ class DataTable
     {
         $query = (method_exists($this->builder, 'getQuery') ? $this->builder->getQuery() : $this->builder);
         $pdo = $query->getConnection()->getPdo();
-        $countStatement = $pdo->prepare('SELECT count(*) as totalCount FROM ('.$query->toSql().') subQuery');
-        $countStatement->execute($query->getBindings());
+
+        try {
+            $countStatement = $pdo->prepare('SELECT count(*) as totalCount FROM ('.$this->builder->toSql().') subQuery');
+            $countStatement->execute($this->builder->getBindings());
+        } catch (Exception $e) {
+            $countStatement = $pdo->prepare('SELECT count(*) as totalCount FROM ('.$query->toSql().') subQuery');
+            $countStatement->execute($query->getBindings());
+        }
+        
         $result = $countStatement->fetch();
         return $result['totalCount'];
     }
