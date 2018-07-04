97aca3c4ffde024f0de2f36964c31b991ab94a2d
Jeffrey de Vreede <jeffrey@devreede.eu>
2018-07-04 09:07:10 +0200

    Remove typeerror testing from phpspec and added php 7.0 and 7.1 to tests.

--- spec/LiveControl/EloquentDataTable/DataTableSpec.php
+++ spec/LiveControl/EloquentDataTable/DataTableSpec.php
@@ -20,11 +20,6 @@ class DataTableSpec extends ObjectBehavior
         $this->shouldHaveType('LiveControl\EloquentDataTable\DataTable');
     }
 
-    function it_should_not_change_its_version_transformer_to_anything_other_then_a_version_transformer()
-    {
-        $this->shouldThrow('\Exception')->during('setVersionTransformer', ['109']);
-    }
-
     function it_should_change_its_version_transformer(Version109Transformer $versionTransformer)
     {
         $this->setVersionTransformer($versionTransformer)->shouldHaveType('LiveControl\EloquentDataTable\DataTable');
